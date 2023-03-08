a2b2b7e5e2555deb04ab72437dda48b281d05e06
Jaroslaw Zolnowski <jaroslaw.zolnowski@gmail.com>
2023-03-08 13:52:21 +0100

    fix(test): apply changes after George's code review

--- e2e/tests/app.e2e.js
+++ e2e/tests/app.e2e.js
@@ -1,11 +1,30 @@
 import { expect, test } from '@playwright/test';
-import { NavBarPage } from '../pages/nav-bar-page';
 import { HomePage } from '../pages/home-page';
+import { NavBarPage } from '../pages/nav-bar-page';
 
 test.describe('AngularJS-Material-Vite app', () => {
+  let errorLogs;
+
   test.beforeEach(async ({ page }) => {
+    errorLogs = [];
+
+    page.on('console', message => {
+      if (message.type() === 'error') {
+        errorLogs.push(message.text());
+      }
+    });
+
+    page.on('pageerror', err => {
+      errorLogs.push(err.message);
+    });
+
     await page.goto('');
   });
+
+  test.afterEach(async () => {
+    expect(errorLogs).toStrictEqual([]);
+  });
+
   test.describe('TopNav', () => {
     test('Navbar', async ({ page }) => {
       const navBarPage = new NavBarPage(page);
@@ -13,12 +32,13 @@ test.describe('AngularJS-Material-Vite app', () => {
       await expect(navBarPage.logo).toHaveAttribute('src', '/assets/img/icon-ng-hotrod-light.svg');
       await expect(navBarPage.header).toHaveText('ng-hotrod');
     });
+
     test('Redirect to GitHub page', async ({ page }) => {
       const navBarPage = new NavBarPage(page);
 
       await navBarPage.clickOnGitHubIcon();
 
-      await expect(navBarPage.githubRepoName).toHaveText('angularjs-material-vite');
+      await expect(page.url()).toBe('https://github.com/xlts-dev/angularjs-material-vite');
     });
   });
 
@@ -33,6 +53,7 @@ test.describe('AngularJS-Material-Vite app', () => {
       );
       await expect(await homePage.featuring.all()).toHaveLength(8);
     });
+
     test('Redirect to Angular Material page', async ({ page }) => {
       const homePage = new HomePage(page);
 
