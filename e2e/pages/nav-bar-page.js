fc1bcf81a93204ecdc25b858e5e6e063ef1909af
Jaroslaw Zolnowski <jaroslaw.zolnowski@gmail.com>
2023-03-08 13:54:54 +0100

    fix(e2e): Remove no needed githubRepoName Locator

--- e2e/pages/nav-bar-page.js
+++ e2e/pages/nav-bar-page.js
@@ -1,13 +1,11 @@
 export class NavBarPage {
   logo;
   header;
-  githubRepoName;
 
   constructor(page) {
     this.page = page;
     this.logo = this.page.getByTestId('ng-hotrod-logo');
     this.header = this.page.getByTestId('header');
-    this.githubRepoName = this.page.getByRole('link', { name: 'angularjs-material-vite' });
   }
 
   async clickOnGitHubIcon() {
